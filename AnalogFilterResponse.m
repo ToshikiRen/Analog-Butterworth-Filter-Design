c90b719e93bed6ed34b32653e24904fe2c33aaa8
ToshikiRen <57465577+ToshikiRen@users.noreply.github.com>
2021-01-04 21:16:34 +0200

    Add files via upload

--- AnalogFilterResponse.m
+++ AnalogFilterResponse.m
@@ -8,7 +8,7 @@ wc_band = wc * 100;
 
 % Create an input
 % Time domain
-t = 0: 0.01: 180;
+t = 0: 0.01: 18;
 % Frequency vector
 w = [1, 10, 100, 200, 300, 400]'; 
 arg = w * t;
@@ -46,7 +46,7 @@ for i = 1:numel(order)
     % Vectorize bode outputs
     amp = amp(:);
     phase = phase(:);
-    semilogx(ax_freq, w, 20 * log10(abs(amp)), 'Color', 'm', 'LineWidth', 3)
+    semilogx(ax_freq, w, 20 * log10(abs(amp)), 'Color', 'k', 'LineWidth', 1)
     grid(ax_freq, 'on');
     xlabel('Frequency');
     ylabel('dB Magnitude');
@@ -55,7 +55,7 @@ for i = 1:numel(order)
     % The response to a sum o sinusoids
     y_out{i} = lsim(H{i}, y, t);
     y_out{i} = y_out{i}(:);
-    plot(ax, t, y, 'LineWidth', 0.5, 'Color', 'c');
+    plot(ax, t, y, 'LineWidth', 0.5, 'Color', [220/255,220/255,220/255]);
     hold(ax, 'on');
     plot(ax, t, y_out{i});
     legend(ax, 'Input Signal', 'Filtered Data');
@@ -68,13 +68,15 @@ for i = 1:numel(order)
         title(ax, [upper(type{i}(1)) type{i}(2:end)  ' filter of order '....
             num2str(order(i)) ' with wc_{start} = ' num2str(wc(i)) ....
             ' and wc_{stop} = ' num2str(wc_band(i)) ' time response']);
+        
         title(ax_freq, [upper(type{i}(1)) type{i}(2:end)  ' filter of order '....
             num2str(order(i)) ' with wc_{start} = ' num2str(wc(i)) ....
             ' and wc_{stop} = ' num2str(wc_band(i)) ' frequency response']);
     else
          title(ax, [upper(type{i}(1)) type{i}(2:end)  ' filter of order '....
             num2str(order(i)) ' with wc = ' num2str(wc(i)) ' time response']);
-         title(ax_freq, [upper(type{i}(1)) type{i}(2:end)  ' filter of order '....
+        
+        title(ax_freq, [upper(type{i}(1)) type{i}(2:end)  ' filter of order '....
             num2str(order(i)) ' with wc = ' num2str(wc(i)) ' frequency response'] );
     end
     
